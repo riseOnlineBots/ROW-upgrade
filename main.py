91041778f4449202b3f8f68f8a37409dd17d315c
Alican BALIK <alicanbalik@outlook.com>
2022-05-23 15:19:09 +0200

    .

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
-from time import time
+import sys
+import threading
+from time import time, sleep
 
 import cv2 as cv
+import pyautogui
+from pynput import keyboard
 
 from vision import Vision
 from windowcapture import WindowCapture
@@ -10,35 +14,60 @@ from windowcapture import WindowCapture
 # Doing this because I'll be putting the files from each video in their own folder on GitHub
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+# for i in list(range(3))[::-1]:
+#     print('Starting in ', i + 1)
+#     sleep(1)
+
 # initialize the WindowCapture class
 wincap = WindowCapture(None)
 # initialize the Vision class
-vision_limestone = Vision('magePad.jpg')
+vision_limestone = Vision('magePad.jpg', 'commonUpgradeScroll.jpg')
 
-'''
-# https://www.crazygames.com/game/guns-and-bottle
-wincap = WindowCapture()
-vision_gunsnbottle = Vision('gunsnbottle.jpg')
-'''
+running = False
 
 loop_time = time()
-while (True):
 
-    # get an updated image of the game
-    screenshot = wincap.get_screenshot()
 
-    # display the processed image
-    points = vision_limestone.find(screenshot, 0.5, 'points')
-    # points = vision_gunsnbottle.find(screenshot, 0.7, 'points')
+def run():
+    while running:
+        while True:
 
-    # debug the loop rate
-    # print('FPS {}'.format(1 / (time() - loop_time)))
-    loop_time = time()
+            # get an updated image of the game
+            screenshot = wincap.get_screenshot()
 
-    # press 'q' with the output window focused to exit.
-    # waits 1 ms every loop to process key presses
-    if cv.waitKey(1) == ord('q'):
-        cv.destroyAllWindows()
-        break
+            # display the processed image
+            # vision_limestone.find(screenshot, 0.7)
+            points = vision_limestone.findUpgradeScroll(screenshot, 0.7)
+
+            if len(points):
+                for (x, y) in points:
+                    pyautogui.moveTo(x, y)
+
+            cv.imshow('Matches', screenshot)
+
+
+def stop():
+    global running
+    running = False
+    sys.exit()
+
+
+def on_press(key):
+    global running
+
+    if key == keyboard.Key.esc:
+        stop()
+        return False  # Stops the listener.
+    try:
+        k = key.char  # Single-char keys.
+    except:
+        k = key.name
+    if k in ['f12']:
+        running = True
+        threading.Thread(target=run).start()
+
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
 
 print('Done.')
--- vision.py
+++ vision.py
@@ -3,42 +3,97 @@ import numpy as np
 
 
 class Vision:
-
     # properties
-    needle_img = None
-    needle_w = 0
-    needle_h = 0
+    upgraded_item_img = None
+    upgraded_item_w = 0
+    upgraded_item_h = 0
+
+    upgrade_scroll = None
+    upgrade_scroll_w = 0
+    upgrade_scroll_h = 0
+
     method = None
 
+    found_upgrade_scroll_w = 0
+    found_upgrade_scroll_h = 0
+
     # constructor
-    def __init__(self, needle_img_path, method=cv.TM_CCOEFF_NORMED):
+    def __init__(self, upgraded_item_path, upgrade_scroll_path, method=cv.TM_CCOEFF_NORMED):
         # load the image we're trying to match
         # https://docs.opencv.org/4.2.0/d4/da8/group__imgcodecs.html
-        self.needle_img = cv.imread(needle_img_path, cv.IMREAD_UNCHANGED)
+        self.upgraded_item_img = cv.imread(upgraded_item_path, cv.IMREAD_UNCHANGED)
+        self.upgrade_scroll_img = cv.imread(upgrade_scroll_path, cv.IMREAD_UNCHANGED)
 
         # Save the dimensions of the needle image
-        self.needle_w = self.needle_img.shape[1]
-        self.needle_h = self.needle_img.shape[0]
+        self.upgraded_item_w = self.upgraded_item_img.shape[1]
+        self.upgraded_item_h = self.upgraded_item_img.shape[0]
+
+        self.upgrade_scroll_w = self.upgrade_scroll_img.shape[1]
+        self.upgrade_scroll_h = self.upgrade_scroll_img.shape[0]
 
         # There are 6 methods to choose from:
         # TM_CCOEFF, TM_CCOEFF_NORMED, TM_CCORR, TM_CCORR_NORMED, TM_SQDIFF, TM_SQDIFF_NORMED
         self.method = method
 
-    def find(self, haystack_img, threshold=0.5, debug_mode=None):
+    def findUpgradeScroll(self, screenshot, threshold):
         # run the OpenCV algorithm
-        result = cv.matchTemplate(haystack_img, self.needle_img, self.method)
+        result = cv.matchTemplate(screenshot, self.upgrade_scroll_img, self.method)
 
         # Get the all the positions from the match result that exceed our threshold
         locations = np.where(result >= threshold)
         locations = list(zip(*locations[::-1]))
-        #print(locations)
+
+        rectangles = []
+        for loc in locations:
+            rect = [int(loc[0]), int(loc[1]), self.upgrade_scroll_w, self.upgrade_scroll_h]
+            # Add every box to the list twice in order to retain single (non-overlapping) boxes
+            rectangles.append(rect)
+            rectangles.append(rect)
+        # Apply group rectangles.
+        # The groupThreshold parameter should usually be 1. If you put it at 0 then no grouping is
+        # done. If you put it at 2 then an object needs at least 3 overlapping rectangles to appear
+        # in the result. I've set eps to 0.5, which is:
+        # "Relative difference between sides of the rectangles to merge them into a group."
+        rectangles, weights = cv.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
+
+        points = []
+        if len(rectangles):
+            # print('Found needle.')
+
+            marker_color = (0, 255, 0)
+            marker_type = cv.MARKER_CROSS
+
+            # Loop over all the rectangles
+            x, y, w, h = rectangles[0]
+            # for (x, y, w, h) in rectangles:
+            # Determine the center position
+            center_x = x + int(w / 2)
+            center_y = y + int(h / 2) + 10
+            # Save the points
+            points.append((center_x, center_y))
+
+            # Draw the center point
+            cv.drawMarker(screenshot, (center_x, center_y),
+                          color=marker_color, markerType=marker_type,
+                          markerSize=40, thickness=2)
+
+        return points
+
+    def find(self, screenshot, threshold=0.5, debug_mode=None):
+        # run the OpenCV algorithm
+        result = cv.matchTemplate(screenshot, self.upgraded_item_img, self.method)
+
+        # Get the all the positions from the match result that exceed our threshold
+        locations = np.where(result >= threshold)
+        locations = list(zip(*locations[::-1]))
+        # print(locations)
 
         # You'll notice a lot of overlapping rectangles get drawn. We can eliminate those redundant
         # locations by using groupRectangles().
         # First we need to create the list of [x, y, w, h] rectangles
         rectangles = []
         for loc in locations:
-            rect = [int(loc[0]), int(loc[1]), self.needle_w, self.needle_h]
+            rect = [int(loc[0]), int(loc[1]), self.upgraded_item_w, self.upgraded_item_h]
             # Add every box to the list twice in order to retain single (non-overlapping) boxes
             rectangles.append(rect)
             rectangles.append(rect)
@@ -52,7 +107,7 @@ class Vision:
 
         points = []
         if len(rectangles):
-            print('Found needle.')
+            # print('Found needle.')
 
             line_color = (0, 255, 0)
             line_type = cv.LINE_4
@@ -63,8 +118,8 @@ class Vision:
             for (x, y, w, h) in rectangles:
 
                 # Determine the center position
-                center_x = x + int(w/2)
-                center_y = y + int(h/2)
+                center_x = x + int(w / 2)
+                center_y = y + int(h / 2)
                 # Save the points
                 points.append((center_x, center_y))
 
@@ -73,17 +128,12 @@ class Vision:
                     top_left = (x, y)
                     bottom_right = (x + w, y + h)
                     # Draw the box
-                    cv.rectangle(haystack_img, top_left, bottom_right, color=line_color,
-                                lineType=line_type, thickness=2)
+                    cv.rectangle(screenshot, top_left, bottom_right, color=line_color,
+                                 lineType=line_type, thickness=2)
                 elif debug_mode == 'points':
                     # Draw the center point
-                    cv.drawMarker(haystack_img, (center_x, center_y),
-                                color=marker_color, markerType=marker_type,
-                                markerSize=40, thickness=2)
-
-        if debug_mode:
-            cv.imshow('Matches', haystack_img)
-            #cv.waitKey()
-            #cv.imwrite('result_click_point.jpg', haystack_img)
+                    cv.drawMarker(screenshot, (center_x, center_y),
+                                  color=marker_color, markerType=marker_type,
+                                  markerSize=40, thickness=2)
 
         return points
